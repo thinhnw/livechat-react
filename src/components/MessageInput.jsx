f644eeba74a35ff7f87e1fda4ff03a081006477d
thinhnw <thinhnv0412@gmail.com>
2024-10-25 11:59:34 +0700

    Send and retrieve message history

--- src/components/MessageInput.jsx
+++ src/components/MessageInput.jsx
@@ -1,10 +1,37 @@
+import { useParams } from "react-router-dom";
+import { apiFetch } from "../utils/api";
+import { useState } from "react";
+
 const MessageInput = () => {
+  const { id } = useParams();
+  const [message, setMessage] = useState("");
+
+  const sendMessage = async (e) => {
+    if (e.key !== "Enter") return;
+    try {
+      const new_message = await apiFetch("/messages", {
+        method: "POST",
+        body: JSON.stringify({
+          content: message,
+          chat_room_id: id
+        }),
+      });
+      if (new_message) {
+        setMessage("");
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
   return (
     <footer className="p-4 border-t">
       <input
         type="text"
         placeholder="Type a message"
         className="w-full bg-gray-100 rounded-3xl py-2 px-4 outline-none"
+        value={message}
+        onChange={(e) => setMessage(e.target.value)}
+        onKeyDown={sendMessage}
       />
     </footer>
   );
--- src/components/Messages.jsx
+++ src/components/Messages.jsx
@@ -1,32 +1,38 @@
 import useFetch from "../hooks/useFetch";
 import { useParams } from "react-router-dom";
+import { useContext } from "react";
+import { UserMeContext } from "../contexts/UserMeContext";
 
 const Messages = () => {
   const { id } = useParams();
   const { data } = useFetch(`/messages?chat_room_id=${id}`);
-  return (  
+  const { state } = useContext(UserMeContext);
+  const { userMe } = state;
+  return (
     <div className="flex-1">
       {data && (
         <ul className="list-none">
-          {data["messages"].map((message) => (
-            <li key={message.id} className="p-6 border-b">
-              <div className="flex items-center p-2 border-b hover:bg-gray-100 cursor-pointer">
-                {/* <img
-                  src={message.user.avatar_url}
-                  alt={message.user.name}
-                  className="w-12 h-12 rounded-full mr-3"
-                /> */}
-                <div className="flex-grow">
-                  <div className="font-semibold">{message.user.display_name}</div>
-                  <div className="text-gray-600">{message.text}</div>
-                </div>
-              </div>
-            </li>
-          ))}
+          {data["messages"]
+            .slice()
+            .reverse()
+            .map((message) => (
+              <li key={message.id} className="p-6 border-b">
+                {message.user_id === userMe._id && (
+                  <div className="flex items-center p-2 justify-end">
+                    <div className="text-gray-600">{message.content}</div>
+                  </div>
+                )}
+                {message.user_id !== userMe._id && (
+                  <div className="flex items-center p-2">
+                    <div className="text-gray-600">{message.content}</div>
+                  </div>
+                )}
+              </li>
+            ))}
         </ul>
       )}
     </div>
   );
-}
- 
+};
+
 export default Messages;
