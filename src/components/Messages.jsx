7f3741cadeb5fd7097fbbc7cea5f400df93e38c8
thinhnw <thinhnv0412@gmail.com>
2024-11-11 15:10:55 +0700

    Add chat bubble

--- src/components/Messages.jsx
+++ src/components/Messages.jsx
@@ -12,22 +12,26 @@ const Messages = ({ messages }) => {
     }
   }, [messages.length]);
   return (
-    <div className="flex-1 overflow-y-auto" ref={messagesRef}>
+    <div className="flex-1 overflow-y-auto p-5" ref={messagesRef}>
       {messages && (
         <ul className="list-none max-h-screen">
           {messages
             .slice()
             .reverse()
             .map((message) => (
-              <li key={message._id} className="p-6 border-b">
+              <li key={message._id} className="mb-1">
                 {message.user_id === userMe._id && (
-                  <div className="flex items-center p-2 justify-end">
-                    <div className="text-gray-600">{message.content}</div>
+                  <div className="flex justify-end">
+                    <div className="bg-violet-400 text-white p-3 rounded-md min-w-[50px] max-w-xs break-words">
+                      {message.content}
+                    </div>
                   </div>
                 )}
                 {message.user_id !== userMe._id && (
-                  <div className="flex items-center p-2">
-                    <div className="text-gray-600">{message.content}</div>
+                  <div className="flex justify-start">
+                    <div className="bg-gray-300 text-black p-3 rounded-md min-w-[50px] max-w-xs break-words">
+                      {message.content}
+                    </div>
                   </div>
                 )}
               </li>
