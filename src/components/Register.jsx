f2d86b57f6436b8f58908269d1e2b1e70f93f26a
thinhnw <thinhnv0412@gmail.com>
2024-11-11 14:39:02 +0700

    Add error ui for auth

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -5,6 +5,7 @@ import { Link } from "react-router-dom";
 const Register = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -19,7 +20,7 @@ const Register = () => {
       console.log(res);
     } catch (error) {
       console.error(error);
-      alert(error.message);
+      setError(error.message);
     } finally {
       setEmail("");
       setPassword("");
@@ -32,13 +33,16 @@ const Register = () => {
         onSubmit={handleSubmit}
         className="w-full mx-auto bg-white p-8 border border-gray-300 shadow-md rounded"
       >
+        {error && <p className="text-red-500 mb-4">{error}</p>}
         <h2 className="text-2xl font-bold mb-4">Create a new account</h2>
         <div className="mb-4">
           <label className="block text-gray-700 font-bold mb-2">Email:</label>
           <input
             type="email"
             value={email}
-            onChange={(e) => setEmail(e.target.value)}
+            onChange={(e) => {
+              setEmail(e.target.value), setError(null);
+            }}
             required
             className="w-full px-3 py-2 border border-gray-300 rounded focus:outline-none focus:ring focus:ring-indigo-200"
           />
@@ -50,7 +54,9 @@ const Register = () => {
           <input
             type="password"
             value={password}
-            onChange={(e) => setPassword(e.target.value)}
+            onChange={(e) => {
+              setPassword(e.target.value), setError(null);
+            }}
             required
             className="w-full px-3 py-2 border border-gray-300 rounded focus:outline-none focus:ring focus:ring-indigo-200"
           />
